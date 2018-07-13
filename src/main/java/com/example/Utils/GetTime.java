15ad5f9c4fdbe86f74100e683dc076b52108e1ea
ZhuZxX <zhuzx72@163.com>
2018-07-13 14:12:14 +0800

    修改获取时间方法

--- src/main/java/com/example/Utils/GetTime.java
+++ src/main/java/com/example/Utils/GetTime.java
@@ -1,14 +1,10 @@
 package com.example.Utils;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class GetTime {
     public static java.sql.Date getTime() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss ");
-        String nowTime = simpleDateFormat.format(new Date());
-        java.sql.Date date = java.sql.Date.valueOf(nowTime);
-
+        java.sql.Date date =new java.sql.Date(new Date().getTime());
         System.out.println(date);
         return date;
     }
